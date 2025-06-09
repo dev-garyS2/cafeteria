bfc39cac8352b7b2746f444bfa27d3e646a4980a
garyS2 <alejandrog.ag40@gmail.com>
2025-06-09 04:54:26 -0400

    Hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,8 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouterRouter basename="/cafeteria">
+    <HashRouter basename="/cafeteria/">
       <App />
-    </HashRouterRouter>
+    </HashRouter>
   </React.StrictMode>
 );
